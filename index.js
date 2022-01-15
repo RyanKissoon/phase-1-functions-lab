8b1c2d6078f7a193453bcac23e2c57a3113d7648
ryankissoon <ryankissoon_31@msn.com>
2022-01-15 11:12:25 -0500

    commit tree c7f59e44c78ab6c6ca4ff42195b55aaa740cb2d9

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Code your solution in this file!
+let hq = 42;
+
+function distanceFromHqInBlocks(blocks){
+   if (blocks > hq){
+        return (blocks - hq);
+   }else if (blocks < hq){
+        return (hq - blocks);
+   }
+};
+
+function distanceFromHqInFeet(blocks){
+    if (blocks > hq){
+        return ((blocks - hq)*264);
+   }else if (blocks < hq){
+        return ((hq - blocks)*264);
+   }
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (start > destination){
+        return ((start - destination)*264);
+   }else if (start < destination){
+        return ((destination - start)*264);
+   }
+}
+
+function calculatesFarePrice(start,destination){
+    const distance = distanceTravelledInFeet(start,destination)
+    if (distance < 400){
+        return 0;
+    }else if (distance >= 400 && distance <= 2000){
+        return ((distance - 400)* 0.02);
+    }else if (distance > 2000 && distance < 2500){
+        return 25;    
+    }else if (distance >= 2500){
+        return 'cannot travel that far'
+    }
+}
